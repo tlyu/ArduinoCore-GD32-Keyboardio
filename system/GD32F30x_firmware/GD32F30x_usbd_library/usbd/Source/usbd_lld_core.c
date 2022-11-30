59fc553f08fad284b113801035c345d91618d355
Taylor Yu <code@argon.blue>
2022-11-30 16:20:20 -0600

    re-apply local USBD library fixes

--- system/GD32F30x_firmware/GD32F30x_usbd_library/usbd/Source/usbd_lld_core.c
+++ system/GD32F30x_firmware/GD32F30x_usbd_library/usbd/Source/usbd_lld_core.c
@@ -418,7 +418,7 @@ static void usbd_ep_stall_clear (usb_dev *udev, uint8_t ep_addr)
             udev->transc_in[ep_num].ep_stall = 0U;
 
             /* clear endpoint stall status */
-            USBD_EP_TX_STAT_SET(ep_num, EPTX_VALID);
+            USBD_EP_TX_STAT_SET(ep_num, EPTX_NAK);
         }
     } else {
         /* don't change status of a disabled endpoint */
@@ -429,7 +429,7 @@ static void usbd_ep_stall_clear (usb_dev *udev, uint8_t ep_addr)
             udev->transc_out[ep_num].ep_stall = 0U;
 
             /* clear endpoint stall status */
-            USBD_EP_RX_STAT_SET(ep_num, EPRX_VALID);
+            USBD_EP_RX_STAT_SET(ep_num, EPRX_NAK);
         }
     }
 }
